dd505575c584142c51b3e95eddc47bd41784eefa
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2020-04-19 22:01:38 -0600

    Updates

---
+++ precache-manifest.7358f96f71494d9b2efc826f4e938630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c5e514ce365dc752afbe3d962b07cb",
+    "url": "/tarot-pwa/index.html"
+  },
+  {
+    "revision": "938bce603e076ce7a9d7",
+    "url": "/tarot-pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "269c6be0484686c8f9d1",
+    "url": "/tarot-pwa/static/js/2.9fe72934.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarot-pwa/static/js/2.9fe72934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938bce603e076ce7a9d7",
+    "url": "/tarot-pwa/static/js/main.7f160b25.chunk.js"
+  },
+  {
+    "revision": "7c0611018353483e4cfa",
+    "url": "/tarot-pwa/static/js/runtime-main.64240f90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tarot-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
